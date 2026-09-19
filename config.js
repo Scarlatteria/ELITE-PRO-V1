a2457d33845fc1db438f7d4239bbfdd704afcb48
Scarlatte <109867667+Scarlatteria@users.noreply.github.com>
2026-09-19 08:21:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,22 +4,22 @@ const chalk = require('chalk');
 
 // Contact details
 global.sessionid = process.env.SESSION_ID || '';
-global.ytname = process.env.YT_NAME || "YT: @EliteProTechs";
-global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
-global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
+global.ytname = process.env.YT_NAME || "YT: -";
+global.socialm = process.env.SOCIAL_M || "GitHub: Scarlatteria";
+global.location = process.env.LOCATION || "Indonesia, Jakarta";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2347047504860';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '6285788390394';
+global.ownername = process.env.OWNER_NAME || 'Scarlatte';
+global.botname = process.env.BOT_NAME || '☁️Columbina Bot☁️';
 
 // Default settings 
 global.prefix = process.env.PREFIX || '.';
 // Settings: true=enable false=disable
-global.autoRecording = process.env.AUTO_RECORDING === 'true';
-global.autoTyping = process.env.AUTO_TYPING === 'true';
-global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
-global.autoread = process.env.AUTO_READ === 'true';
+global.autoRecording = process.env.AUTO_RECORDING === 'false';
+global.autoTyping = process.env.AUTO_TYPING === 'false';
+global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'false';
+global.autoread = process.env.AUTO_READ === 'false';
 global.autobio = process.env.AUTO_BIO !== 'false';
 global.autoviewstatus = process.env.AUTO_VIEW_STATUS !== 'false';
 global.welcome = process.env.WELCOME !== 'false';
@@ -34,10 +34,10 @@ global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
 // Sticker details
 global.packname = process.env.PACKNAME || 'Sticker By';
-global.author = process.env.AUTHOR || 'EliteProTech\n\nContact: +2347047504860';
+global.author = process.env.AUTHOR || 'Scarlatte\n\nContact: +6285788390394';
 // Default settings 2
-global.wm = process.env.WM || "Youtube @EliteProTechs";
-global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g';
+global.wm = process.env.WM || "Youtube -";
+global.link = process.env.LINK || '-';
 
 // Reply messages
 global.mess = {
